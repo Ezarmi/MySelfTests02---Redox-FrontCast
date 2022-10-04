96a4b854528b22f7f4a9ec44a2e003051a4b266e
احسان ضرغامی <ezarmi@gmail.com>
2022-10-04 05:59:02 +0330

    145th Section

--- App.js
+++ App.js
@@ -7,10 +7,25 @@ const initialState = {
 }
 
 function rootReducer (state=initialState, action){
+    if (action.type==="INC"){
+        return{
+            ...state,
+            counter: state.counter+1
+        }
+    }
+    if (action.type==='ADD'){
+        return{
+            ...state,
+            counter: state.counter+action.value,
+        }
+    }
     return state
 }
 const store =createStore(rootReducer)
 
 // console.log(store.getState())
 
-store.dispatch({ type: "" })
+store.dispatch({ type: "INC" })
+store.dispatch({ type: "ADD", value: 2 })
+
+console.log(store.getState())
